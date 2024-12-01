2516b783971302651b2cf2504c11099934103e22
MBWhite <whitemat@uk.ibm.com>
2024-12-01 08:38:04 +0000

    setup
    Signed-off-by: MBWhite <whitemat@uk.ibm.com>

---
+++ 00/ts/src/main.ts
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import sourceMapSupport from 'source-map-support';
+sourceMapSupport.install();
+
+import chalk from 'chalk';
+import fs from 'fs';
+import { World } from './world.js';
+import { logger } from './services/logger.js';
+
+const w = new World();
+const pj = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+
+console.log(`Running ${chalk.green.bold(pj.description)}`);
+console.log(`Hello ${chalk.blue(w.getText())}`);
+
+logger.info('Hello');
